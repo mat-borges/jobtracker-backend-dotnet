7f238667d412e8d69a2dc2cf7a9c6088b702c22d
Mateus Borges <borgesm.work@gmail.com>
2025-09-29 17:06:16 -0300

    feat: ModelState validation and handle exceptions

--- src/JobTracker.Api/Controllers/AuthController.cs
+++ src/JobTracker.Api/Controllers/AuthController.cs
@@ -16,17 +16,35 @@ namespace JobTracker.Api.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserRegisterDto dto)
+        public async Task<IActionResult> Register([FromBody]UserRegisterDto dto)
         {
-            var user = await _authService.RegisterAsync(dto);
-            return Ok(new { user.Email, user.Id });
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var user = await _authService.RegisterAsync(dto);
+                return Ok(new { user.Email, user.Id });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login(UserLoginDto dto)
+        public async Task<IActionResult> Login([FromBody]UserLoginDto dto)
         {
-            var authResponse = await _authService.LoginAsync(dto);
-            return Ok(authResponse);
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                var authResponse = await _authService.LoginAsync(dto);
+                return Ok(authResponse);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
         }
     }
 }
